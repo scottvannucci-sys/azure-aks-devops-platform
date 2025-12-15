41cebb535320f09e291952bc180185c1ddb3304c
Scott Vannucci <scott.vannucci@gmail.com>
2025-12-14 23:06:31 -0600

    Initial commit: AKS platform scaffolding and infra pipeline

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,93 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.100"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+}
+
+# Networking
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.20.0.0/16"]
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "${var.prefix}-snet-aks"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.20.1.0/24"]
+}
+
+# Log Analytics
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = "${var.prefix}-law"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+# ACR
+resource "azurerm_container_registry" "acr" {
+  name                = replace("${var.prefix}acr", "-", "")
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
+
+# AKS
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "${var.prefix}-aks"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.prefix}-dns"
+
+  default_node_pool {
+    name           = "system"
+    node_count     = var.node_count
+    vm_size        = var.node_vm_size
+    vnet_subnet_id = azurerm_subnet.aks.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    network_policy = "azure"
+  }
+
+  depends_on = [azurerm_log_analytics_workspace.law]
+}
+
+# Allow AKS to pull images from ACR
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
---
+++ terraform/environments/dev/variables.tf
@@ -0,0 +1,24 @@
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "prefix" {
+  type    = string
+  default = "sv-aksplat-dev"
+}
+
+variable "kubernetes_version" {
+  type    = string
+  default = null
+}
+
+variable "node_count" {
+  type    = number
+  default = 2
+}
+
+variable "node_vm_size" {
+  type    = string
+  default = "Standard_D2s_v5"
+}
